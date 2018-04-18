ee8e5748313f3aec322bb786f92cd8f0735b7ac1
root <root@rbodewaldt12763>
2018-04-18 16:44:56 +0000

    Final Project - 2018

--- themes/twentyseventeen/footer.php
+++ themes/twentyseventeen/footer.php
@@ -21,19 +21,23 @@
 				<?php
 				get_template_part( 'template-parts/footer/footer', 'widgets' );
 
-				if ( has_nav_menu( 'social' ) ) : ?>
+				if ( has_nav_menu( 'social' ) ) :
+				?>
 					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
 						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'social',
-								'menu_class'     => 'social-links-menu',
-								'depth'          => 1,
-								'link_before'    => '<span class="screen-reader-text">',
-								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-							) );
+							wp_nav_menu(
+								array(
+									'theme_location' => 'social',
+									'menu_class'     => 'social-links-menu',
+									'depth'          => 1,
+									'link_before'    => '<span class="screen-reader-text">',
+									'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+								)
+							);
 						?>
 					</nav><!-- .social-navigation -->
-				<?php endif;
+				<?php
+				endif;
 
 				get_template_part( 'template-parts/footer/site', 'info' );
 				?>
@@ -45,8 +49,3 @@
 
 </body>
 </html>
-<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-			<div id="widget-area" class="widget-area" role="complementary">
-				<?php dynamic_sidebar( 'sidebar-1' ); ?>
-			</div><!-- .widget-area -->
-		<?php endif; ?>
